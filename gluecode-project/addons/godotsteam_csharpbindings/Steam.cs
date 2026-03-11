1b022b2d45c4ee4d4e09f6e79badb5127aa251e7
Laura Sofia Heimann <hello@laura.media>
2026-03-11 12:45:40 +0100

    Update persona state methods to use `PersonaState` enum
    Refactored `GetFriendPersonaState` and `GetPersonaState` methods in C# bindings to return the `PersonaState` enum instead of `long`. Adjusted related patcher operations to ensure compatibility.

--- gluecode-project/addons/godotsteam_csharpbindings/Steam.cs
+++ gluecode-project/addons/godotsteam_csharpbindings/Steam.cs
@@ -7996,8 +7996,8 @@ public enum RemoteStoragePlatform
     public new string GetFriendPersonaNameHistory(long steamId, long nameHistory) => 
         Call(GDExtensionMethodName.GetFriendPersonaNameHistory, [steamId, nameHistory]).As<string>();
 
-    public new long/* "Empty Enum Constant String" */ GetFriendPersonaState(long steamId) => 
-        Call(GDExtensionMethodName.GetFriendPersonaState, [steamId]).As<long/* "Empty Enum Constant String" */>();
+public new PersonaState GetFriendPersonaState(long steamId) =>
+Call(GDExtensionMethodName.GetFriendPersonaState, [steamId]).As<PersonaState>();
 
     public new long/* "Empty Enum Constant String" */ GetFriendRelationship(long steamId) => 
         Call(GDExtensionMethodName.GetFriendRelationship, [steamId]).As<long/* "Empty Enum Constant String" */>();
@@ -8038,8 +8038,8 @@ public enum RemoteStoragePlatform
     public new string GetPersonaName() => 
         Call(GDExtensionMethodName.GetPersonaName, []).As<string>();
 
-    public new long/* "Empty Enum Constant String" */ GetPersonaState() => 
-        Call(GDExtensionMethodName.GetPersonaState, []).As<long/* "Empty Enum Constant String" */>();
+public new PersonaState GetPersonaState() =>
+Call(GDExtensionMethodName.GetPersonaState, []).As<PersonaState>();
 
     public new void GetPlayerAvatar(long size = 2, long steamId = 0) => 
         Call(GDExtensionMethodName.GetPlayerAvatar, [size, steamId]);
